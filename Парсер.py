718681fafcfacd0b0bcc9de5f01a5bf7e61705fc
Boris Dvorkin <b_dvorkin@mail.ru>
2022-07-14 01:40:05 +0300

    Parser for ready_to_parse file.
    cntrl+c, cntrl+v --> done.

---
+++ Парсер.py
@@ -0,0 +1,19 @@
+with open('ПрогИнж.txt', 'r') as file:
+    text = file.read().splitlines()
+
+contest = []
+olymp = 0
+for x in range(len(text)):
+    l = [x for x in text[x].split('\t') if x != '']
+    if l[2] == 'Нет':
+        contest.append(int(l[3]) + int(l[4]) + int(l[5]))
+    else:
+        olymp += 1
+
+contest.sort()
+contest.reverse()
+print('--БВИ--\n')
+print(olymp)
+print('\n\n--ОБЩИЙ КОНКУРС--\n\n')
+print(contest)
+
